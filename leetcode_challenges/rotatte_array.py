bcb69c509869600d2567dcf2cafa9fbec8c81c2f
Jonathan Kamau <jonathan.kamau@andela.com>
2019-10-30 14:16:36 +0300

    [Rotate Array] add rotate array pythonic solution

---
+++ leetcode_challenges/rotatte_array.py
@@ -0,0 +1,32 @@
+'''
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+
+Input: [1,2,3,4,5,6,7] and k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+'''
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
+        pointer = 0
+        for index in range(0, len(nums)):
+                number = nums[-k]
+                nums.pop(-k)
+                nums.insert(pointer, number)
+                
+                pointer += 1
+                k-=1
+                
+                if k == 0:
+                    break
+        
